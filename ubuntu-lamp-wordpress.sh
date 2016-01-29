43fc38b8e8ef80a3428a75c53342af9f52aa7282
qianji <qianji.zheng@gmail.com>
2016-01-29 10:03:40 -0600

    Update ubuntu-lamp-wordpress.sh
    install wordpress

--- ubuntu-lamp-wordpress.sh
+++ ubuntu-lamp-wordpress.sh
@@ -1,3 +1,4 @@
+http://www.chrishjorth.com/blog/free-aws-ec2-ubuntu-apache-php-mysql-setup/
 #This is a script for setting up AWS EC2 Ubuntu-Apache-PHP-MySQL and wordpress
 sudo apt-get update
 sudo apt-get dist-upgrade -y
@@ -16,3 +17,21 @@ sudo apt-get install mysql-server -y
 sudo apt-get install php5-mysql -y
 
 #sudo apt-get install phpmyadmin -y
+
+sudo mysql -u root -p123456 -e "CREATE DATABASE wordpress;"
+sudo mysql -u root -p123456 -e "CREATE USER wordpressuser@localhost IDENTIFIED BY 'password';"
+sudo mysql -u root -p123456 -e "GRANT ALL PRIVILEGES ON wordpress.* TO wordpressuser@localhost;"
+sudo mysql -u root -p123456 -e "FLUSH PRIVILEGES;"
+
+cd ~
+wget http://wordpress.org/latest.tar.gz
+sudo tar xzvf latest.tar.gz
+
+sudo apt-get install php5-gd libssh2-php
+cd ~/wordpress
+cp wp-config-sample.php wp-config.php
+sudo rsync -avP ~/wordpress/ /var/www/html/
+sudo chown -R ubuntu:www-data *
+mkdir /var/www/html/wp-content/uploads
+sudo chown -R :www-data /var/www/html/wp-content/uploads
+
