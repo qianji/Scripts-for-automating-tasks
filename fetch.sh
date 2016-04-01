98cd789d52b8575a7f9cabba2ea9aa6503bda849
qianji <qianji.zheng@gmail.com>
2016-03-31 23:23:35 -0500

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,12 @@
-if [ -f  /home/bitnami/htdocs/auto-pull/git-pull-requests/example.org ]
-then
-rm -f  /home/bitnami/htdocs/auto-pull/git-pull-requests/example.org
-cd /srv/shiny-server/sample-apps/data-analysis-shiny/ && sudo git pull
-cd /srv/shiny-server/sample-apps/ggcmecon/ && sudo git pull ggcmecon
+sudo rm -f  /var/www/html/auto-pull/git-pull-requests/example.org
+#sudo rm -rf /srv/shiny-server/data-analysis-shiny/
+#sudo rm -rf /srv/shiny-server/ggcmecon/
+cd /srv/shiny-server/data-analysis-shiny/
+#sudo git clone https://github.com/mrbsoftisms/ggcmecon.git
+sudo git pull
+cd /srv/shiny-server/ggcmecon/
+#sudo git clone https://github.com/qianji/data-analysis-shiny.git
+sudo git pull
+cd /srv/shiny-server/Scripts-for-automating-tasks/
+sudo git pull
 fi
